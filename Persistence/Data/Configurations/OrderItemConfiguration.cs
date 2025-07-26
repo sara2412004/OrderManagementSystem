a105102ddccf7ea18ff2c6735877cd8678692d74
sara farid moussa <sarafarid02@gmail.com>
2025-07-27 02:30:51 +0300

    create layers, entities,dbContext,Migrations

---
+++ Persistence/Data/Configurations/OrderItemConfiguration.cs
@@ -0,0 +1,28 @@
+﻿using Domain.Models;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Persistence.Data.Configurations
+{
+    internal class OrderItemConfiguration : IEntityTypeConfiguration<OrderItem>
+    {
+        public void Configure(EntityTypeBuilder<OrderItem> builder)
+        {
+            builder.HasOne(O => O.Order)
+                   .WithMany(i => i.OrderItems)
+                   .HasForeignKey(O => O.OrderId);
+
+            builder.HasOne(P => P.Product)
+                   .WithMany(i => i.OrderItems)
+                   .HasForeignKey(P => P.ProductId);
+
+            builder.Property(oi => oi.UnitPrice).HasPrecision(18, 2);
+            builder.Property(oi => oi.Discount).HasPrecision(18, 2);
+        }
+    }
+}
