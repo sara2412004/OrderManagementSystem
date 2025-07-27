a116d2934c98b074c8376a332647611af1136b24
sara farid moussa <sarafarid02@gmail.com>
2025-07-27 12:46:11 +0300

    Repositories

---
+++ Domain/Contracts/IGenericRepository.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Contracts
+{
+    public interface IGenericRepository<TEntity> where TEntity : class
+    {
+        public Task<IEnumerable<TEntity>> GetAllAsync();
+        public Task<TEntity?> GetByIdAsync(int id);
+        public Task AddAsync(TEntity entity);
+        public void Remove(TEntity entity);
+        public void Update(TEntity entity);
+
+    }
+}
